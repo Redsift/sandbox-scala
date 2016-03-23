904e480aebc260270951c1a88e05ff7985aeb064
Deepak Prabhakara <deepak.prabhakara@gmail.com>
2016-03-23 00:28:24 +0000

    re-organised jar files

--- test/server/Node1.scala
+++ test/server/Node1.scala
@@ -4,7 +4,7 @@ import com.redsift.ComputeResponse;
 package server {
     object Node1 {
       def compute(req: ComputeRequest): ComputeResponse = {
-        println(req.toString());
+        println("Node1.scala: " + req.toString());
         val res = new ComputeResponse("bucket-scala", "key-scala", "value-scala", 0);
         return res;
       }
--- test/server/Node2.scala
+++ test/server/Node2.scala
@@ -3,7 +3,7 @@ import com.redsift.ComputeResponse;
 
 object Node2 {
   def compute(req: ComputeRequest): Array[ComputeResponse] = {
-    println(req.toString());
+    println("Node2.scala: " + req.toString());
     val res = new ComputeResponse("bucket-scala2", "key-scala2", "value-scala2", 0);
     return Array[ComputeResponse](res);
   }
